4f0fcb39f30eb5dad71c2df0bfd1dc4ed364488e
Henzley <140068322+Henzley@users.noreply.github.com>
2026-06-14 15:27:18 +0200

    Complete exercise 10 of foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+// If year is divisible by 4, it is a leap year.
+// If year is divisible by 100 it is not a leap year, unless it is divisible by 400.
 
+const leapYears = function (year) {
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
