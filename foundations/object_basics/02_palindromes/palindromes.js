7b0a0633f0966ddd05890f66da72b31610589c5a
Henzley Spogter <140068322+Henzley@users.noreply.github.com>
2026-09-21 19:06:35 +0200

    Complete palindrome exercise.

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const validCharacters = "acdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleansedWord = word
+    .toLowerCase()
+    .split("")
+    .filter((letter) => validCharacters.includes(letter))
+    .join("");
+
+  const reversedWord = cleansedWord.split("").reverse().join("");
+
+  if (cleansedWord === reversedWord) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
