98f0895329d31b97985463dbaf9e7276d2591c69
Henzley Spogter <140068322+Henzley@users.noreply.github.com>
2026-09-22 17:01:55 +0200

    Complete findTheOldest exercise.

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+//get age if person has no yearOfDeath
+const getAge = function (death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+
+    return oldestAge < currentAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
