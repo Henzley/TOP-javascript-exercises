073a651e3838eefa3146e1662c94983747cbded3
Henzley Spogter <140068322+Henzley@users.noreply.github.com>
2026-09-22 15:21:11 +0200

    Complete fibonacci sequence exercise.

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0 || typeof num !== "number" || Number.isNaN(num)) {
+    return "OOPS";
+  }
 
+  if (num === 0) {
+    return 0;
+  } else {
+    let num1 = 1;
+    let num2 = 0;
+    for (let i = 2; i <= num; i++) {
+      let current = num1 + num2;
+      num2 = num1;
+      num1 = current;
+    }
+    return num1;
+  }
 };
 
 // Do not edit below this line
